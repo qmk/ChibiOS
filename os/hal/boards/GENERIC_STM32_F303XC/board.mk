d34e8eb83101a95f98892bf68605fe545821f320
Jack Humbert <jack.humb@gmail.com>
2017-07-20 00:23:25 -0400

    adds generic f303xc

---
+++ os/hal/boards/GENERIC_STM32_F303XC/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = ${CHIBIOS}/os/hal/boards/GENERIC_STM32_F303XC/board.c
+
+# Required include directories
+BOARDINC = ${CHIBIOS}/os/hal/boards/GENERIC_STM32_F303XC
