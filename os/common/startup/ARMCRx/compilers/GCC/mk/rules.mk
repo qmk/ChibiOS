7a87f0cfb643a7ab0fdbf34d41065bf62016ba0f
gdisirio <gdisirio@085d63ac-f8f8-4fb4-a5e5-c089fd4f3ff4>
2026-05-21 08:09:55 +0000

    Add ARMCRx startup FPU enable
    git-svn-id: https://svn.code.sf.net/p/chibios/code/trunk@18232 085d63ac-f8f8-4fb4-a5e5-c089fd4f3ff4

--- os/common/startup/ARMCRx/compilers/GCC/mk/rules.mk
+++ os/common/startup/ARMCRx/compilers/GCC/mk/rules.mk
@@ -1,3 +1,21 @@
 # ARM Cortex-Rx common makefile scripts and rules.
 
 include $(CHIBIOS)/os/common/startup/ARM/compilers/GCC/rules.mk
+
+# FPU options default.
+ifeq ($(USE_FPU_OPT),)
+  USE_FPU_OPT = -mfloat-abi=$(USE_FPU) -mfpu=vfpv3-d16
+endif
+
+# FPU-related options.
+ifeq ($(USE_FPU),)
+  USE_FPU = no
+endif
+ifneq ($(USE_FPU),no)
+  OPT    += $(USE_FPU_OPT)
+  DDEFS  += -DCORTEX_USE_FPU=TRUE
+  DADEFS += -DCORTEX_USE_FPU=TRUE
+else
+  DDEFS  += -DCORTEX_USE_FPU=FALSE
+  DADEFS += -DCORTEX_USE_FPU=FALSE
+endif
