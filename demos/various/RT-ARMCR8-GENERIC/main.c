07b06cdfc97078fdbb6b7d5269a0f978b0873f96
gdisirio <gdisirio@085d63ac-f8f8-4fb4-a5e5-c089fd4f3ff4>
2026-05-21 14:24:58 +0000

    Add temporary GIC timer support to ARMCR8 demo
    git-svn-id: https://svn.code.sf.net/p/chibios/code/trunk@18237 085d63ac-f8f8-4fb4-a5e5-c089fd4f3ff4

--- demos/various/RT-ARMCR8-GENERIC/main.c
+++ demos/various/RT-ARMCR8-GENERIC/main.c
@@ -14,7 +14,26 @@
     limitations under the License.
 */
 
+#include "armcr8.h"
 #include "ch.h"
+
+#define R8_RENODE_PRIVATE_TIMER_BASE 0xAE000600U
+#define R8_RENODE_PRIVATE_TIMER_IRQ  29U
+#define R8_RENODE_PRIVATE_TIMER_FREQ 667000000U
+
+#define PTIMER_CONTROL_ENABLE        (1U << 0)
+#define PTIMER_CONTROL_AUTO_RELOAD   (1U << 1)
+#define PTIMER_CONTROL_IRQ_ENABLE    (1U << 2)
+#define PTIMER_ISR_EVENT             (1U << 0)
+
+typedef struct {
+  volatile uint32_t LOAD;
+  volatile uint32_t COUNTER;
+  volatile uint32_t CONTROL;
+  volatile uint32_t ISR;
+} r8_private_timer_t;
+
+#define R8_PRIVATE_TIMER ((r8_private_timer_t *)R8_RENODE_PRIVATE_TIMER_BASE)
 
 static volatile uint32_t thread_counter;
 static volatile uint32_t main_counter;
@@ -23,6 +42,51 @@ static volatile uint32_t fpu_thread_counter;
 static volatile uint32_t fpu_main_counter;
 static volatile uint32_t fpu_error_counter;
 #endif
+
+/*
+ * Demo-private IRQ dispatcher.
+ */
+bool __port_irq_dispatch(void) {
+  IRQn_Type irqn;
+  bool preemption_required;
+
+  preemption_required = false;
+  irqn = GIC_AcknowledgePending();
+
+  if (irqn == (IRQn_Type)R8_RENODE_PRIVATE_TIMER_IRQ) {
+    R8_PRIVATE_TIMER->ISR = PTIMER_ISR_EVENT;
+
+    chSysLockFromISR();
+    chSysTimerHandlerI();
+    preemption_required = chSchIsPreemptionRequired();
+    chSysUnlockFromISR();
+  }
+
+  GIC_EndInterrupt(irqn);
+
+  return preemption_required;
+}
+
+static void gic_init(void) {
+
+  GIC_Enable();
+  GIC_SetPriority((IRQn_Type)R8_RENODE_PRIVATE_TIMER_IRQ, 0x80U);
+  GIC_SetTarget((IRQn_Type)R8_RENODE_PRIVATE_TIMER_IRQ, 1U);
+  GIC_EnableIRQ((IRQn_Type)R8_RENODE_PRIVATE_TIMER_IRQ);
+}
+
+static void timer_init(void) {
+  uint32_t interval;
+
+  interval = (R8_RENODE_PRIVATE_TIMER_FREQ / CH_CFG_ST_FREQUENCY) - 1U;
+
+  R8_PRIVATE_TIMER->CONTROL = 0U;
+  R8_PRIVATE_TIMER->ISR     = PTIMER_ISR_EVENT;
+  R8_PRIVATE_TIMER->LOAD    = interval;
+  R8_PRIVATE_TIMER->CONTROL = PTIMER_CONTROL_ENABLE |
+                              PTIMER_CONTROL_AUTO_RELOAD |
+                              PTIMER_CONTROL_IRQ_ENABLE;
+}
 
 /*
  * RTOS example thread.
@@ -35,8 +99,8 @@ static THD_FUNCTION(Thread1, arg) {
   chRegSetThreadName("counter");
 
   while (true) {
+    chThdSleepMilliseconds(100);
     thread_counter++;
-    chThdYield();
   }
 }
 
@@ -58,7 +122,7 @@ static THD_FUNCTION(Thread2, arg) {
 
   while (true) {
     __asm volatile ("" : "+w" (marker));
-    chThdYield();
+    chThdSleepMilliseconds(10);
     __asm volatile ("" : "+w" (marker));
 
     if (marker != expected) {
@@ -81,6 +145,9 @@ int main(void) {
   register double marker asm ("d8");
   double expected;
 #endif
+
+  gic_init();
+  timer_init();
 
   /*
    * System initialization, the main() function becomes a thread and the
@@ -110,7 +177,7 @@ int main(void) {
     __asm volatile ("" : "+w" (marker));
 #endif
     main_counter++;
-    chThdYield();
+    chThdSleepMilliseconds(200);
 #if CORTEX_USE_FPU == TRUE
     __asm volatile ("" : "+w" (marker));
 
